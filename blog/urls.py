552b3a53c1a6c1271424fefd7542ee4e4a03bd24
harpreetsingh <harpreet162004@gmail.com>
2025-07-08 10:00:27 +0530

    Blog app

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+from . import views
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:post_id>/share/', views.share_file, name='share-file'),
+    path('search/', views.search, name='search'),
+    path('users/search/', views.user_search, name='user-search'),
+    path('about/', views.about, name='blog-about'),
+]
